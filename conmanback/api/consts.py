9397853cf1a6e2afb2977f21afc9f6d26ba25b35
sylvain <sylvaingossou@gmail.com>
2025-06-10 12:01:12 +0000

    Updating with the remote version

---
+++ conmanback/api/consts.py
@@ -0,0 +1,8 @@
+
+PHASE_PRESELECTION = "préselection"
+PHASE_PREALABLE = "préalable"
+PHASE_ECRITE = "écrite"
+PHASE_TERMINE = "terminé"
+
+BAC2_LIBELLE = "Baccalauréat Deuxième Partie"
+BAC1_LIBELLE = "Baccalauréat Première Partie"
